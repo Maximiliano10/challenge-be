a09edbbc9071b6ec8204151f624f7afba42947a0
maxi <rodriguez.maxi@ymail.com>
2023-03-23 12:32:59 -0300

    limpieza

--- api-calculate/src/main/java/com/api/calculate/service/ApiCalculateService.java
+++
@@ -1,6 +0,0 @@
-package com.api.calculate.service;
-
-public interface ApiCalculateService {
-
-	Float getProcessCalculate(Float n1, Float n2);
-}
--- api-core/src/main/java/com/api/core/service/ApiCoreService.java
+++
@@ -1,6 +0,0 @@
-package com.api.core.service;
-
-public interface ApiCoreService {
-
-	Float getCalculate(Float n1, Float n2);
-}
